9871eda0f3ca9592962cee4fcfaa50038b88b4df
jniechcial <jakub.niechcial@gmail.com>
2014-02-28 12:46:03 +0100

    Finish sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 RegistrationPage::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',  to: 'users#new',            via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
