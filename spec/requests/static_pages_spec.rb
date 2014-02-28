fa5ab45f6c9a3444979e7086f9b24063f041d5ff
jniechcial <jakub.niechcial@gmail.com>
2014-02-28 09:54:15 +0100

    Small rspec refactor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,17 +4,19 @@ describe "Static pages" do
 
 	subject { page } 
 
+	let(:base_title) { "Cyberbot Robotics Festival 2014" }
+
   describe "Home page" do
   	before { visit '/static_pages/home' }
 
     it { should have_content("Registration for Cyberbot Robotics Festival") }
-    it { should have_selector("title", text: "Cyberbot Robotics Festival 2014 | Home" )}
+    it { should have_selector("title", text: "#{base_title} | Home" )}
   end
 
   describe "Contact page" do
   	before { visit '/static_pages/contact' }
 
   	it { should have_content("Contact") }
-  	it { should have_selector("title", text: "Cyberbot Robotics Festival 2014 | Contact" )}
+  	it { should have_selector("title", text: "#{base_title} | Contact" )}
   end
 end
