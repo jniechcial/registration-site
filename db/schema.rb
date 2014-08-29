2b498b3d68cb6fa57043a7d621dc70cfc3f8ab38
jniechcial <jakub.niechcial@gmail.com>
2014-08-29 20:05:45 +0200

    migration run

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308152653) do
+ActiveRecord::Schema.define(version: 20140827193822) do
 
   create_table "competitions", force: true do |t|
     t.string   "name"
@@ -54,14 +54,24 @@ ActiveRecord::Schema.define(version: 20140308152653) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",                  default: false
     t.string   "city"
     t.string   "tshirt"
     t.boolean  "terms"
     t.boolean  "agreement"
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
