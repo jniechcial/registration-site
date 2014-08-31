74f2a9879a18053e947b90053f4412f00fe697e3
jniechcial <jakub.niechcial@gmail.com>
2014-08-31 17:37:18 +0200

    robots controlled switched to devise helpers

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,5 +1,5 @@
 class RobotsController < ApplicationController
-	before_filter :signed_in_user, only: [:new]
+	before_filter :authenticate_user!, only: [:new]
 	before_filter :correct_user_for_edit_and_show, only: [:show, :edit]
 	before_filter :correct_user_for_update_and_create, only: [:udate, :create]
 	before_action :admin_user, only: [:destroy]
@@ -49,16 +49,16 @@ class RobotsController < ApplicationController
 		end
 
 		def correct_user_for_edit_and_show
-			if signed_in?
+			if user_signed_in?
 				@team = Robot.find_by_id(params[:id]).team
 				redirect_to root_path unless @team.users.include?(current_user)
 			else
-				redirect_to signin_path
+				redirect_to new_user_session_path
 			end
 		end
 
 		def correct_user_for_update_and_create
-			if signed_in?
+			if user_signed_in?
 				@team = Team.find_by_id(params[:robot][:team_id])
 				if @team
 					redirect_to root_path unless @team.users.include?(current_user)
